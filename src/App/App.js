42842955811e09758444ab32a1c1733940cdfe5f
Adriane Sutherland <asutherland91@gmail.com>
2023-05-30 10:02:28 -0700

    Add correct api url

--- src/App/App.js
+++ src/App/App.js
@@ -13,7 +13,7 @@ class App extends Component {
   }
 
   componentDidMount() {
-    fetch('http://localhost:3001')
+    fetch('http://localhost:3001/api/v1/reservations')
       .then((response) => response.json())
       .then((data) => {
         this.setState({
