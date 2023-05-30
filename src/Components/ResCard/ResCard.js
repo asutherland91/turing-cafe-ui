a2437d8e3d8c894d7827ef3dec1f3b7413437fd3
Adriane Sutherland <asutherland91@gmail.com>
2023-05-30 09:11:19 -0700

    Add updated rescard retup cause I remembered how it should look

--- src/Components/ResCard/ResCard.js
+++ src/Components/ResCard/ResCard.js
@@ -2,13 +2,13 @@ import React from "react";
 import "./ResCard.css";
 
 
-export const ReservationsDisplay = ({reservations}) => {
+export const ReservationsDisplay = ({name, date, time, number}) => {
   return (
     <div>
-      <h3>{reservations.name}</h3>
-      <p>{reservations.date}</p>
-      <p>{reservations.time}</p>
-      <p>{reservations.number}</p>
+      <h3>{name}</h3>
+      <p>{date}</p>
+      <p>{time}</p>
+      <p>Number of Guests: {number}</p>
       <button>Cancel</button>
     </div>
   )
