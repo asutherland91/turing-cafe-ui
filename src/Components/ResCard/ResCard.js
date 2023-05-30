c3e58d8276f80f57fe60961b1d4574ecaf63ff34
Adriane Sutherland <asutherland91@gmail.com>
2023-05-30 09:04:06 -0700

    Add skeleton for reservation card

--- src/Components/ResCard/ResCard.js
+++ src/Components/ResCard/ResCard.js
@@ -1,2 +1,15 @@
 import React from "react";
 import "./ResCard.css";
+
+
+export const ReservationsDisplay = ({reservations}) => {
+  return (
+    <div>
+      <h3>{reservations.name}</h3>
+      <p>{reservations.date}</p>
+      <p>{reservations.time}</p>
+      <p>{reservations.number}</p>
+      <button>Cancel</button>
+    </div>
+  )
+}
