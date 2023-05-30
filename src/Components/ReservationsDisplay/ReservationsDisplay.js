a854231df723f94e777e046d623728fc968ec3c0
Adriane Sutherland <asutherland91@gmail.com>
2023-05-30 09:20:34 -0700

    Add skeleton to the resevation display component

--- src/Components/ReservationsDisplay/ReservationsDisplay.js
+++ src/Components/ReservationsDisplay/ReservationsDisplay.js
@@ -1,3 +1,14 @@
 import React from "react";
 import ResCard from "./ResCard";
 import "./ReservationsDisplay.css";
+
+export const ReservationsDisplay = ({reservations}) => {
+  const resCards = reservations.map(reservation => {
+    <ResCard
+      name={reservation.name}
+      date={reservation.date}
+      time={reservation.time}
+      number={reservation.number}
+      />
+  })
+}
