c23e0bec6fdabf4bc6f254e009651e5a85b88b14
Adriane Sutherland <asutherland91@gmail.com>
2023-05-30 09:29:44 -0700

    Add fetch request to App component

--- src/App/App.js
+++ src/App/App.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
 import './App.css';
+import { ReservationsDisplay } from '../Components/ResCard/ResCard';
 
 
 
 class App extends Component {
   constructor() {
     super();
+    this.state = {
+      reservations: []
+    };
   }
 
+  componentDidMount() {
+    fetch('http://localhost:3001')
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({
+          reservations: data
+        });
+      });
+    }
+
   render() {
+    const reservations = this.state
     return (
       <div className="App">
         <h1 className='app-title'>Turing Cafe Reservations</h1>
@@ -16,7 +31,7 @@ class App extends Component {
 
         </div>
         <div className='resy-container'>
-          
+          <ReservationsDisplay reservations={reservations} />
         </div>
       </div>
     )
